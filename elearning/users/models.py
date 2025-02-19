64689bbad7a8d7365319ecf68d4ba528f33ea899
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2025-02-19 18:07:14 +0800

    feat(user): add user registration function

--- elearning/users/models.py
+++ elearning/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class AppUser(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  is_student = models.BooleanField(default=False)
+  is_teacher = models.BooleanField(default=False)
+  real_name = models.CharField(max_length=150)
+  profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)
+  bio = models.TextField(blank=True)
--- elearning/users/views.py
+++ elearning/users/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from .forms import *
 
-# Create your views here.
+def register(request):
+  registered = False
+
+  if request.method == 'POST':
+    user_form = UserForm(data=request.POST)
+    profile_form = UserProfileForm(data=request.POST)
+
+    if user_form.is_valid and profile_form.is_valid():
+      user = user_form.save()
+      user.set_password(user.password)
+      user.save()
+
+      profile = profile_form.save(commit=False)
+      profile.user = user
+
+      user_role = profile_form.cleaned_data['user_role']
+      if user_role == 'student':
+        profile.is_student = True
+        profile.is_teacher = False
+      elif user_role == 'teacher':
+        profile.is_teacher = True
+        profile.is_student = False
+
+      if 'real_name' in request.POST:
+        profile.real_name = request.POST['real_name']
+
+      if 'profile_picture' in request.FILES:
+        profile.profile_picture = request.FILES['profile_picture']
+
+      if 'bio' in request.POST:
+        profile.bio = request.POST['bio']
+
+      profile.save()
+      registered = True
+
+  else:
+    user_form = UserForm()
+    profile_form = UserProfileForm()
+
+  return render(request, 'users/register.html', {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
