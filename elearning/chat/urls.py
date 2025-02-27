766c7fc7cea95c01a2fd17bdb5380fecd1548349
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2025-02-27 18:58:49 +0800

    refactor(chat): update file and function names

--- elearning/chat/urls.py
+++ elearning/chat/urls.py
@@ -3,6 +3,6 @@ from django.contrib.auth.decorators import login_required
 from chat import views as chat_views
 
 urlpatterns = [
-  path('', login_required(login_url='/user/login/')(chat_views.index), name='index'),
-  path('chat/<str:room_name>/', login_required(login_url='/user/login/')(chat_views.chat_room), name='chat_room'),
+  path('', login_required(login_url='/user/login/')(chat_views.search_room), name='search_room'),
+  path('<str:room_name>/', login_required(login_url='/user/login/')(chat_views.chat_room), name='chat_room'),
 ]
