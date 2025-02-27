721ef9349319065626c5dfc19870077500a1cf69
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2025-02-27 16:16:15 +0800

    fix(urls): fix login required url

--- elearning/users/urls.py
+++ elearning/users/urls.py
@@ -8,11 +8,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('register/', user_views.user_register, name='register'),
     path('login/', user_views.user_login, name='login'),
-    path('logout/', login_required(login_url='login/')(user_views.user_logout), name='logout'),
-    path('home/', login_required(login_url='login/')(user_views.homepage), name='home'),
-    path('search/', login_required(login_url='login/')(user_views.search_users), name='search_users'),
-    path('search/<int:user_id>/', login_required(login_url='login/')(user_views.show_profile), name='show_profile'),
-    path('notification/', login_required(login_url='login/')(user_views.all_notifications), name='all_notifications'),
-    path('profile/', login_required(login_url='login/')(user_views.user_profile), name='user_profile'),
-    path('profile/edit/', login_required(login_url='login/')(user_views.edit_profile), name='edit_profile'),
+    path('logout/', login_required(login_url='/user/login/')(user_views.user_logout), name='logout'),
+    path('home/', login_required(login_url='/user/login/')(user_views.homepage), name='home'),
+    path('search/', login_required(login_url='/user/login/')(user_views.search_users), name='search_users'),
+    path('search/<int:user_id>/', login_required(login_url='/user/login/')(user_views.show_profile), name='show_profile'),
+    path('notification/', login_required(login_url='/user/login/')(user_views.all_notifications), name='all_notifications'),
+    path('profile/', login_required(login_url='/user/login/')(user_views.user_profile), name='user_profile'),
+    path('profile/edit/', login_required(login_url='/user/login/')(user_views.edit_profile), name='edit_profile'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
