077ba396e84c92c0bd73e5dece976e435b75ca12
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2025-02-28 22:33:37 +0800

    feat(chat): enhance chat functiona with user profile photos and timestamps

--- elearning/chat/consumers.py
+++ elearning/chat/consumers.py
@@ -1,6 +1,8 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-
+from django.utils import timezone
+from asgiref.sync import sync_to_async
+from users.models import AppUser
 class ChatConsumer(AsyncWebsocketConsumer):
   async def connect(self):
     self.room_name = self.scope['url_route']['kwargs']['room_name']
@@ -21,18 +23,39 @@ class ChatConsumer(AsyncWebsocketConsumer):
   async def receive(self, text_data):
     text_data_json = json.loads(text_data)
     message = text_data_json['message']
+    user = self.scope['user']
+    username = user.username
+    profile_photo_url = await self.get_profile_photo_url(user)
+    time_sent = timezone.now().strftime('%d-%m-%Y %H:%M:%S ')
 
     await self.channel_layer.group_send(
       self.room_group_name,
       {
         'type': 'chat_message',
-        'message': message
+        'message': message,
+        'username': username,
+        'profile_photo_url': profile_photo_url,
+        'time_sent': time_sent
       }
     )
 
   async def chat_message(self, event):
     message = event['message']
+    username = event['username']
+    profile_photo_url = event['profile_photo_url']
+    time_sent = event['time_sent']
 
     await self.send(text_data=json.dumps({
-      'message': message
+      'message': message,
+      'username': username,
+      'profile_photo_url': profile_photo_url,
+      'time_sent': time_sent
     }))
+
+  @sync_to_async
+  def get_profile_photo_url(self, user):
+    try:
+      app_user = AppUser.objects.get(user=user)
+      return app_user.profile_photo.url
+    except AppUser.DoesNotExist:
+      return "/media/default_profile.jpg"
--- elearning/chat/urls.py
+++ elearning/chat/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 from chat import views as chat_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
   path('', login_required(login_url='/user/login/')(chat_views.search_room), name='search_room'),
   path('<str:room_name>/', login_required(login_url='/user/login/')(chat_views.chat_room), name='chat_room'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
