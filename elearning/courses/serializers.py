118a31dfb34a84f347b59133c3c448002baadb40
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2025-03-01 16:48:59 +0800

    feat(serializers): update serializers

--- elearning/courses/serializers.py
+++ elearning/courses/serializers.py
@@ -1,11 +1,32 @@
 from rest_framework import serializers
-from .models import Course
-from users.models import AppUser
+from .models import Course, CourseFeedback, CourseMaterial, Notification
+from users.serializers import AppUserSerializer
 
 class CourseSerializer(serializers.ModelSerializer):
-  created_by = serializers.StringRelatedField()
-  enrolled_students = serializers.StringRelatedField(many=True)
+  created_by = AppUserSerializer(read_only=True)
+  enrolled_students = AppUserSerializer(many=True, read_only=True)
 
   class Meta:
     model = Course
-    field = ['id', 'course_title', 'course_description', 'created_by', 'enrolled_students', 'time_created']
+    fields = ['id', 'course_title', 'course_description', 'time_created', 'created_by', 'enrolled_students']
+
+class CourseFeedbackSerializer(serializers.ModelSerializer):
+  student = AppUserSerializer(read_only=True)
+
+  class Meta:
+    model = CourseFeedback
+    fields = ['id', 'course', 'student', 'feedback_content', 'time_submitted']
+
+class CourseMaterialSerializer(serializers.ModelSerializer):
+  uploaded_by = AppUserSerializer(read_only=True)
+
+  class Meta:
+    model = CourseMaterial
+    fields = ['id', 'course', 'uploaded_by', 'original_name', 'title', 'file', 'upload_time']
+
+class NotificationSerializer(serializers.ModelSerializer):
+  user = AppUserSerializer(read_only=True)
+
+  class Meta:
+    model = Notification
+    fields = ['id', 'user', 'message', 'time_created', 'read']
