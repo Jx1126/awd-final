0cc7c6a7fee4f42f701b6fe48463effd2df200dd
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2025-03-02 15:44:37 +0800

    feat(api): add OpenAPI schema endpoint

--- elearning/api/urls.py
+++ elearning/api/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from .views import *
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
-  path('user/profile/', UserProfileView.as_view(), name='user-profile-api'),
-  path('user/status-updates/', UserStatusUpdateView.as_view(), name='user-status-updates-api'),
-  path('user/enrolled-courses/', UserEnrolledCoursesView.as_view(), name='user-enrolled-courses-api'),
-  path('user/created-courses/', UserCreatedCoursesView.as_view(), name='user-created-courses-api'),
-  path('user/notifications/', UserNotificationView.as_view(), name='user-notifications-api'),
+  path('schema/', get_schema_view(title="eLearning Application", description="API that allows users to access their data", version="1.0"), name='openapi_schema'),
 
-  path('course/<int:course_id>/feedback/', CourseFeedbackView.as_view(), name='course-feedback-api'),
-  path('course/<int:course_id>/materials/', CourseMaterialView.as_view(), name='course-materials-api'),
+  path('user/profile/', UserProfileView.as_view(), name='user_profile_api'),
+  path('user/status-updates/', UserStatusUpdateView.as_view(), name='user_status_updates_api'),
+  path('user/enrolled-courses/', UserEnrolledCoursesView.as_view(), name='user_enrolled_courses_api'),
+  path('user/created-courses/', UserCreatedCoursesView.as_view(), name='user_created_courses_api'),
+  path('user/notifications/', UserNotificationView.as_view(), name='user_notifications_api'),
+
+  path('course/<int:course_id>/feedback/', CourseFeedbackView.as_view(), name='course_feedback_api'),
+  path('course/<int:course_id>/materials/', CourseMaterialView.as_view(), name='course_materials_api'),
 ]
