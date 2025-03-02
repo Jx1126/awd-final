f1ba35b2a5cb7b63d3cef4fe5748b9f2aea5090d
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2025-03-02 15:02:46 +0800

    refactor(admin): remove unused admin files and register additional models

--- elearning/courses/admin.py
+++ elearning/courses/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, CourseFeedback, CourseMaterial, Notification
 
 admin.site.register(Course)
+admin.site.register(CourseFeedback)
+admin.site.register(CourseMaterial)
+admin.site.register(Notification)
--- elearning/users/admin.py
+++ elearning/users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import AppUser, UserStatusUpdate
 
 admin.site.register(AppUser)
+admin.site.register(UserStatusUpdate)
