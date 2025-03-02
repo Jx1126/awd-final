71bc1e99b3796fb37d7eff082c504c3ef3a6199b
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2025-03-02 16:05:49 +0800

    feat(api): add Swagger for API documentation

--- elearning/api/urls.py
+++ elearning/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import *
 from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
   path('schema/', get_schema_view(title="eLearning Application", description="API that allows users to access their data", version="1.0"), name='openapi_schema'),
+  path('swagger/', TemplateView.as_view(template_name='swagger.html', extra_context={'schema_url': 'openapi_schema'}), name='swagger_ui'),
 
   path('user/profile/', UserProfileView.as_view(), name='user_profile_api'),
   path('user/status-updates/', UserStatusUpdateView.as_view(), name='user_status_updates_api'),
